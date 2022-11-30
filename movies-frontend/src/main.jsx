bf3fc26625d037004bcb5b0fc3ea885b1d514b17
Ramiro Andres Bedoya Escobar <iamramiroo@gmail.com>
2022-11-30 07:31:17 -0500

    first commit

--- movies-frontend/src/main.jsx
+++ movies-frontend/src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hi There</div>,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
-);
+)
