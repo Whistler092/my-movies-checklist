b1d5c7b9015f32978daeb99f79dac587505c5ab3
Ramiro Andres Bedoya Escobar <iamramiroo@gmail.com>
2022-12-01 09:15:53 -0500

    load films with fake database

--- movies-frontend/src/routes/movies.jsx
+++ movies-frontend/src/routes/movies.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./movies.scss";
+
 export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("db.json")
+      .then((res) => setMovies(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
-      <h2>Movies</h2>
+      <div className="films">
+        {movies &&
+          movies?.results?.map((film) => <Film key={film.id} film={film} />)}
+      </div>
     </>
+  );
+}
+
+function Film({ film }) {
+  return (
+    <div key={film.id} className="film">
+      <img
+        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${film.poster_path}`}
+      ></img>
+      <span>{film.original_title}</span>
+    </div>
   );
 }
